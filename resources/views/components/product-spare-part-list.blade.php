618088ba66530dc9e60734e285be214aeb43c6f2
Fran Rey <franreycastedo@gmail.com>
2025-12-03 18:22:07 +0100

    add: show old price

--- resources/views/components/product-spare-part-list.blade.php
+++ resources/views/components/product-spare-part-list.blade.php
@@ -3,13 +3,18 @@
     @foreach ($relatedSpareparts as $spare_part)
         <li class="p-1 flex gap-2 items-center">
             <div class="flex-1 flex items-center text-left">⚙️ - {{ $spare_part->name }}</div>
+            <div class="flex items-center text-left font-bold">
                 @if($spare_part->price_with_discount)
-                    <div class="flex items-center text-left font-bold text-green-600">
-                    {{ $spare_part->getFormattedPriceWithDiscount() }}
-                @else
-                    <div class="flex items-center text-left font-bold">
-                    {{ $spare_part->getFormattedPrice() }}
+                    <span class="text-green-600">
+                        {{ $spare_part->getFormattedPriceWithDiscount() }}
+                    </span>
+                    &nbsp;
                 @endif
+                <span @class([
+                    'line-through' => $spare_part->price_with_discount
+                ])>
+                    {{ $spare_part->getFormattedPrice() }}
+                </span>
             </div>
             @livewire('buttons.product-cart-buttons', ['product' => $spare_part, collect() ])
         </li>
