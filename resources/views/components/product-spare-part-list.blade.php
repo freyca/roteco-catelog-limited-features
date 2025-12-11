d5fad99511ac3c1173c890cba68e63f081a25369
Fran Rey <franreycastedo@gmail.com>
2025-12-11 17:22:20 +0100

    add: price styling

--- resources/views/components/product-spare-part-list.blade.php
+++ resources/views/components/product-spare-part-list.blade.php
@@ -1,20 +1,14 @@
 <div class="ml-3">
     <ul>
     @foreach ($relatedSpareparts as $spare_part)
-        <div class="grid grid-cols-[1fr_auto] md:grid-cols-[1fr_auto_auto] gap-1">
+        <div class="py-2 grid grid-cols-[1fr_auto] md:grid-cols-[1fr_auto_auto] gap-1 border-b border-gray-300">
             <div
-                class="p-2
-                    row-start-1
-                    md:col-auto md:row-auto
-                    flex items-center">
+                class="text-left row-start-1 md:col-auto md:row-auto flex items-center">
                 ⚙️ - {{ $spare_part->name }}
             </div>
 
             <div
-                class="p-2 font-bold
-                    row-start-2
-                    md:col-auto md:row-auto
-                    flex items-center">
+                class="mr-2 font-bold row-start-2 md:col-auto md:row-auto flex items-center">
                 @if($spare_part->price_with_discount)
                     <span class="text-green-600">
                         {{ $spare_part->getFormattedPriceWithDiscount() }}
@@ -29,11 +23,7 @@
             </div>
 
             <div
-                class="p-2
-                    col-start-2 row-span-2
-                    flex items-center
-                    w-max
-                    md:col-auto md:row-span-1 md:w-auto md:flex-none">
+                class="col-start-2 row-span-2 flex items-center w-max md:col-auto md:row-span-1 md:w-auto md:flex-none">
                 @livewire('buttons.product-cart-buttons', ['product' => $spare_part, collect() ])
             </div>
         </div>
