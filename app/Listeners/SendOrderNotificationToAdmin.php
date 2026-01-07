a07612004315fa102adbd5a02015d47efa45a101
Fran Rey <franreycastedo@gmail.com>
2026-01-07 17:42:12 +0100

    fix: admin notifications

--- app/Listeners/SendOrderNotificationToAdmin.php
+++ app/Listeners/SendOrderNotificationToAdmin.php
@@ -16,10 +16,11 @@ class SendOrderNotificationToAdmin implements ShouldQueue
     {
         $admin_notification_email = config('custom.mail_admin_address');
 
-        if (is_null($$admin_notification_email)) {
+        if (is_null($admin_notification_email)) {
             throw new RuntimeException('No admin users found. Please ensure at least one admin user exists in the system.');
         }
 
-        Notification::send($admin_notification_email, new AdminOrderNotification($event->order));
+        Notification::route('mail', $admin_notification_email)
+            ->notify(new AdminOrderNotification($event->order));
     }
 }
